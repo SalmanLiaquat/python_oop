5dded5484cc8e0b966c66a4afe496e29dc12f0b7
SalmanLiaqat3 <129200506+SalmanLiaqat3@users.noreply.github.com>
2025-09-20 10:20:30 +0500

    Modify file

--- classes.py
+++ classes.py
@@ -5,16 +5,16 @@
 #CLASS : blueprint for creating objects (USE TO DESIGN THE STRUCTURE AND LAYOUT OF THE OBJECT)
 #OBJECT : instance of a class
 
-
+'''
 class Car:
     manufacture_year = 2024 # CLASS ATTRIBUTE
     def __init__(self,name,color,model,for_sale): # CONSTRUCTOR
-        self.name =name
+        self.name =name # self->target your instance
         self.color =color
         self.model =model
         self.for_sale =for_sale
 
-    def drive(self,  age):
+    def drive(self,  age): # instance Method
         self.age = age
         if self.age>=18:
             print(f"you can drive {self.name}")
@@ -31,3 +31,33 @@ obj1 =Car("BMW","Black","X5",False)
 obj1.describe()
 obj1.drive(18)
 print(f"Manufacturing Year: {obj1.manufacture_year}")
+
+'''
+
+#class methods-> work with class itself, not target the instance of class(object),
+# we  use @classmethod decorator to create a class method it take cls as the first parameter
+class Animal:
+    def __init__(self,name):
+        self.name = name
+
+    @classmethod
+    def create_animal(cls,name): #cls ->target the class
+        return cls(name)
+    
+    @staticmethod
+    def make_sound():
+        print("Animal makes a sound")
+
+
+animal1 = Animal.create_animal("Dog")
+print(animal1.name)
+animal2 = Animal.create_animal("cat")
+print(animal2.name)
+animal1.make_sound()
+
+
+
+
+#static methods->not target the class and its instance of class(object), 
+# use @staticmethod decorator to create a static method it just a normal regular function
+
---
+++ inheritance.py
@@ -0,0 +1,3 @@
+# INHERITANCE: A class that inherits from another class is called a subclass.
+  # -> help to increase the code reusability and extensibility
+# A class that is inherited from is called a superclass.
